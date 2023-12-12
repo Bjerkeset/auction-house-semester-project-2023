bd316cd236ab896a0411a53dab4955ef5804389c
Bjerkeset <bb.bjerk@gmail.com>
2023-12-12 08:07:47 +0100

    quickfix

--- src/components/shared/reusable/ProfileAvatar.tsx
+++ src/components/shared/reusable/ProfileAvatar.tsx
@@ -3,14 +3,14 @@ import {Avatar, AvatarFallback, AvatarImage} from "@/components/ui/avatar";
 type ProfileAvatarProps = {
   profileImage: string | undefined;
   username: string | undefined;
-  // credits: number;
+  credits?: number;
 };
 
 export default function ProfileAvatar({
   profileImage,
   username,
-}: // credits,
-ProfileAvatarProps) {
+  credits,
+}: ProfileAvatarProps) {
   return (
     <div className="flex items-center gap-1 ">
       <Avatar className="h-8 w-8">
