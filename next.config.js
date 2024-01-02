0e6b9ed54acb2f046bf3e5a51f566d367b5ced03
Bjerkeset <bb.bjerk@gmail.com>
2024-01-02 09:56:53 +0100

    changes to next.config

--- next.config.js
+++ next.config.js
@@ -1,12 +1,51 @@
-/** @type {import('next').NextConfig} */
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   images: {
+//     remotePatterns: [
+//       {
+//         protocol: "https",
+//         hostname: "**",
+//       },
+//     ],
+//   },
+// };
+
+// module.exports = nextConfig;
+
+const path = require("path");
+
+/**
+ * @type {import('next').NextConfig}
+ */
 const nextConfig = {
-  images: {
-    remotePatterns: [
+  // You can add appDir here if it's supported by your Next.js version
+  // appDir: 'app',
+
+  async headers() {
+    return [
       {
-        protocol: "https",
-        hostname: "**",
+        source: "/api/:path*",
+        headers: [
+          {key: "Access-Control-Allow-Credentials", value: "true"},
+          {key: "Access-Control-Allow-Origin", value: "*"},
+          {
+            key: "Access-Control-Allow-Methods",
+            value: "GET,OPTIONS,PATCH,DELETE,POST,PUT",
+          },
+          {
+            key: "Access-Control-Allow-Headers",
+            value:
+              "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
+          },
+          {key: "Content-Type", value: "application/json"}, // Ensure JSON data can be sent
+          {key: "Accept", value: "application/json"}, // Ensure JSON responses are accepted
+        ],
       },
-    ],
+    ];
+  },
+
+  sassOptions: {
+    includePaths: [path.join(__dirname, "styles")],
   },
 };
 
