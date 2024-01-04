fd033687f07d669c341774adfef68f1095007c76
Bjerkeset <bb.bjerk@gmail.com>
2024-01-04 11:03:37 +0100

    next config fix

--- next.config.js
+++ next.config.js
@@ -1,52 +1,52 @@
-// /** @type {import('next').NextConfig} */
-// const nextConfig = {
-//   images: {
-//     remotePatterns: [
-//       {
-//         protocol: "https",
-//         hostname: "**",
-//       },
-//     ],
-//   },
-// };
-
-// module.exports = nextConfig;
-
-const path = require("path");
-
-/**
- * @type {import('next').NextConfig}
- */
+/** @type {import('next').NextConfig} */
 const nextConfig = {
-  // You can add appDir here if it's supported by your Next.js version
-  // appDir: 'app',
-
-  async headers() {
-    return [
+  images: {
+    remotePatterns: [
       {
-        source: "/api/:path*",
-        headers: [
-          {key: "Access-Control-Allow-Credentials", value: "true"},
-          {key: "Access-Control-Allow-Origin", value: "*"},
-          {
-            key: "Access-Control-Allow-Methods",
-            value: "GET,OPTIONS,PATCH,DELETE,POST,PUT",
-          },
-          {
-            key: "Access-Control-Allow-Headers",
-            value:
-              "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
-          },
-          {key: "Content-Type", value: "application/json"}, // Ensure JSON data can be sent
-          {key: "Accept", value: "application/json"}, // Ensure JSON responses are accepted
-        ],
+        protocol: "https",
+        hostname: "**",
       },
-    ];
-  },
-
-  sassOptions: {
-    includePaths: [path.join(__dirname, "styles")],
+    ],
   },
 };
 
 module.exports = nextConfig;
+
+// const path = require("path");
+
+// /**
+//  * @type {import('next').NextConfig}
+//  */
+// const nextConfig = {
+//   // You can add appDir here if it's supported by your Next.js version
+//   // appDir: 'app',
+
+//   async headers() {
+//     return [
+//       {
+//         source: "/api/:path*",
+//         headers: [
+//           {key: "Access-Control-Allow-Credentials", value: "true"},
+//           {key: "Access-Control-Allow-Origin", value: "*"},
+//           {
+//             key: "Access-Control-Allow-Methods",
+//             value: "GET,OPTIONS,PATCH,DELETE,POST,PUT",
+//           },
+//           {
+//             key: "Access-Control-Allow-Headers",
+//             value:
+//               "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
+//           },
+//           {key: "Content-Type", value: "application/json"}, // Ensure JSON data can be sent
+//           {key: "Accept", value: "application/json"}, // Ensure JSON responses are accepted
+//         ],
+//       },
+//     ];
+//   },
+
+//   sassOptions: {
+//     includePaths: [path.join(__dirname, "styles")],
+//   },
+// };
+
+// module.exports = nextConfig;
